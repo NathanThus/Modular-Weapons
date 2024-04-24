566459b314e2184e8fd8850b12d732528637e165
NathanThus <nathan.thus@outlook.com>
2024-04-24 11:03:42 +0200

    ⚡️ Improve performance by avoiding double calculations

--- Assets/Modular Weapon Framework/Scripts/Spread/DiamondSpread.cs
+++ Assets/Modular Weapon Framework/Scripts/Spread/DiamondSpread.cs
@@ -9,8 +9,9 @@ namespace ModularWeapons.Spread
 
         public override Vector2 GetSpread()
         {
-            float x = Random.Range(0,_horizontalSpread);
-            float y = Random.Range(0, _verticalSpread - ((_verticalSpread - x) / 100  * _verticalSpread));
+            float x = Random.Range(-_horizontalSpread,_horizontalSpread);
+            float verticalMax = _verticalSpread - ((_verticalSpread - x) / 100  * _verticalSpread);
+            float y = Random.Range(-verticalMax, verticalMax);
             return new Vector2(x,y);
         }
     }
