574150af98fd5d28cad5f19e6eeed5a6d3e27416
NathanThus <nathan.thus@outlook.com>
2024-04-25 13:45:13 +0200

    ✨ Implement Ammo Reserves

--- Assets/Modular Weapon Framework/Scripts/Weapon/Magazine.cs
+++ Assets/Modular Weapon Framework/Scripts/Weapon/Magazine.cs
@@ -8,25 +8,64 @@ namespace ModularWeapons.Weapon
     [Serializable]
     public class Magazine
     {
+        #region Serialized Fields
+
         [SerializeField] private int _magazineSize;
         [SerializeField] private int _remainingBullets;
+        [SerializeField] private int _reserveAmmo;
 
         [SerializeField] private float _reloadTime;
-        
+
         [SerializeField] private bool _loadSingleBullet;
+
+        #endregion
+
+        #region Fields
 
         private bool _isReloading;
 
-        public async void Reload(CancellationToken token)
+        #endregion
+
+        #region Public
+
+        /// <summary>
+        /// Reloads the magazine with new bullets. Will reload the entire mag, or as much available ammo as possible.
+        /// </summary>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns></returns>
+        public async UniTask Reload(CancellationToken token)
         {
-            if(_magazineSize == _remainingBullets) return;
-            if(_isReloading) return;
+            await Reload(token, _magazineSize);
+        }
+
+        /// <summary>
+        /// Reloads the magazine with new bullets. Will reload the designated amount of bullets, or as much available ammo as possible.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="bullets"></param>
+        /// <returns></returns>
+        public async UniTask Reload(CancellationToken token, int bullets)
+        {
+            if (_magazineSize == _remainingBullets) return;
+            if (_reserveAmmo == 0) return;
+            if (_isReloading) return;
 
             _isReloading = true;
 
             await UniTask.WaitForSeconds(_reloadTime, cancellationToken: token);
 
-            _remainingBullets = _magazineSize;
+            if(bullets > _reserveAmmo)
+            {
+                _remainingBullets = _reserveAmmo;
+                _reserveAmmo = 0;
+            }
+            else
+            {
+                _remainingBullets = bullets;
+                _reserveAmmo -= bullets;
+            }
+
+
             _isReloading = false;
         }
 
@@ -37,18 +76,20 @@ namespace ModularWeapons.Weapon
 
         public bool Fire()
         {
-            if(_remainingBullets <= 0) return false;
-            
+            if (_remainingBullets <= 0) return false;
+
             _remainingBullets--;
             return true;
         }
 
         public bool Fire(int bullets)
         {
-            if(_remainingBullets <= 0 || bullets > _remainingBullets) return false;
+            if (_remainingBullets <= 0 || bullets > _remainingBullets) return false;
 
             _remainingBullets -= bullets;
             return true;
         }
+
+        #endregion
     }
 }
